7c0eeffc399d426312649090b23d619ef617b0bd
Wayne adrianne obeso <obesowayne@gmail.com>
2024-05-08 06:23:22 +0800

    adding featured status and timesince, truncatewords

--- blog_main/views.py
+++ blog_main/views.py
@@ -5,12 +5,14 @@ def home(request):
     # Fetching categories from your database
     categories = Category.objects.all()
     
-    # Fetching featured blog posts using object filter
-    featured_posts = Blog.objects.filter(is_featured=True)
+    # Fetching featured blog posts using object filter with status
+    featured_posts = Blog.objects.filter(is_featured=True, status="published")
+    posts = Blog.objects.filter(is_featured=False, status="published")
     
     # Sending your data into your HTML or frontend
     context = { 
         "categories": categories,
         "featured_posts": featured_posts,
+        "posts": posts,
     }
     return render(request, 'index.html', context)
