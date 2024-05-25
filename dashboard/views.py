6bc79eb70677ef7e594f86a0aa2cbfa570269956
Wayne adrianne obeso <obesowayne@gmail.com>
2024-05-25 08:30:38 +0800

    added category count and login required

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from blogs_app.models import Blog, Category
 
+#login required decorator
+from django.contrib.auth.decorators import login_required
+
+#
+@login_required(login_url='login')
 #This view is for dashboard
 def dashboard(request):
-    return render(request, "dashboard/dashboard.html")
+    category_count = Category.objects.all().count()
+    blogs_count = Blog.objects.all().count()
+    
+    context = {
+        "category_count":category_count,
+        "blogs_count":blogs_count,
+    }
+    return render(request, "dashboard/dashboard.html", context)
