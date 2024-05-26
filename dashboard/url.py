b72f2c62a287a3c7a9b073b718116880aef4fb82
Wayne adrianne obeso <obesowayne@gmail.com>
2024-05-27 06:18:00 +0800

    added sidebar and linking

--- dashboard/url.py
+++ dashboard/url.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     #this path for dashbaord with emptry string
-    path('', views.dashboard, name="dashboard")
+    path('', views.dashboard, name="dashboard"),
+    
+    #sidebar and liniking
+    path("categories/", views.categories, name="categories"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,8 +4,9 @@ from blogs_app.models import Blog, Category
 #login required decorator
 from django.contrib.auth.decorators import login_required
 
-#
+#login required during user going to login
 @login_required(login_url='login')
+
 #This view is for dashboard
 def dashboard(request):
     category_count = Category.objects.all().count()
@@ -16,3 +17,7 @@ def dashboard(request):
         "blogs_count":blogs_count,
     }
     return render(request, "dashboard/dashboard.html", context)
+
+#sidebar and linking
+def categories(request):
+    return render(request, "dashboard/categories.html")
